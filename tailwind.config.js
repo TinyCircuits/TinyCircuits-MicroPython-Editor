dfaad40e65d73f0a07b14788d73455dbbf982d75
jmtinycircuits <jmarcum@tinycircuits.com>
2022-10-12 10:28:27 -0400

    APK arcade now has scrolling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
             './dist/modules/sprite-editor/**/*.{html,js}'],
   theme: {
     extend: {},
+    screens: {
+      'tiny': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [require("daisyui")],
 }

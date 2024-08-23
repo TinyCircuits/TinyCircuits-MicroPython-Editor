8f1725011e6f930a5a4c919c03b4a21d971a7bff
TinyCircuits <info@tinycircuits.com>
2024-08-23 10:43:35 -0400

    Restart

--- dist/js/common/prototypes.js
+++
@@ -1,8 +0,0 @@
-// Prototypes that are not implemented in JavaScript
-
-// Array Remove - By John Resig (MIT Licensed) https://stackoverflow.com/a/9815010
-Array.prototype.remove = function(from, to) {
-    var rest = this.slice((to || from) + 1 || this.length);
-    this.length = from < 0 ? this.length + from : from;
-    return this.push.apply(this, rest);
-};
--- dist/modules/sprite-editor/sprite-editor-split-view.js
+++
@@ -1,7 +0,0 @@
-class SpriteEditorSplitView{
-    constructor(){
-        this.divSpriteList = document.getElementById("divSpriteList");
-        this.divSpriteDraggable = document.getElementById("divSpriteDraggable");
-        this.divSpriteEditorParent = document.getElementById("divSpriteEditorParent");
-    }
-}
